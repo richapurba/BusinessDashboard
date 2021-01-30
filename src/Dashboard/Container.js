db402fbdde088c9c7145141dd3324b0c481848da
Richa Ulivia Hilda Srianty Purba <richathorne@gmail.com>
2021-01-30 19:23:50 +0000

    Add basic responsive style to the head in dashboard and remove the border

--- src/Dashboard/Container.js
+++ src/Dashboard/Container.js
@@ -4,5 +4,9 @@ import styled from "styled-components";
 export const Container = styled.div`
 	max-width: 1200px;
 	margin: auto;
-	border: 1px solid blue;
+
+	@media (max-width: 1200px) {
+		margin: 40px 20px;
+		font-size: 12px;
+	}
 `
