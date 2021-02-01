aa2a97075cbf40d9477e3ad0bf0a801d04f56318
Richa Ulivia Hilda Srianty Purba <richathorne@gmail.com>
2021-02-01 00:33:50 +0000

    Add highcharts bar example

--- src/Dashboard/Chart.js
+++ src/Dashboard/Chart.js
@@ -4,12 +4,41 @@ import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 
 const options = {
-  title: {
-    text: 'My chart'
-  },
-  series: [{
-    data: [1, 2, 3]
-  }]
+    chart: {
+		height: '360px',
+        type: 'column'
+    },
+    title: {
+        text: 'Stacked column chart'
+    },
+    xAxis: {
+        categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Total fruit consumption'
+        }
+    },
+    tooltip: {
+        pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.percentage:.0f}%)<br/>',
+        shared: true
+    },
+    plotOptions: {
+        column: {
+            stacking: 'percent'
+        }
+    },
+    series: [{
+        name: 'John',
+        data: [5, 3, 4, 7, 2]
+    }, {
+        name: 'Jane',
+        data: [2, 2, 3, 2, 1]
+    }, {
+        name: 'Joe',
+        data: [3, 4, 4, 2, 5]
+    }]
 }
 
 export function Chart() {
