626633614d0d2503792838873ab50dcd9b3df0cf
Richa Ulivia Hilda Srianty Purba <richathorne@gmail.com>
2021-02-02 06:56:49 +0000

    Add table data

--- src/Dashboard/Data.js
+++ src/Dashboard/Data.js
@@ -11,17 +11,47 @@ export const chartData = [
         name: 'BTC',
         data: generateData(12, 30, 5),
 		color: '#00698F',
-    }, {
+    },
+	{
         name: 'ETX',
         data: generateData(12, 7, 0.3),
 		color: '#0096cc',
-    }, {
+    },
+	{
         name: 'RXL',
         data: generateData(12, 6, 0.5),
 		color: '#00B4F5',
-    }, {
+    },
+	{
         name: 'ETH',
         data: generateData(12, 5, 0.2),
 		color: '#47CEFF',
     }
 ]
+
+const months = [
+	'January',
+	'February',
+	'March',
+	'April',
+	'May',
+	'June',
+	'July',
+	'August',
+	'September',
+	'October',
+	'November',
+	'December'
+];
+
+function format(num){
+	return `$$(num) Billion`;
+}
+
+export const tableRows = months.map((month, i) => ({
+	month,
+	BTC: format(chartData[0].data[i]),
+	ETX: format(chartData[1].data[i]),
+	RXL: format(chartData[2].data[i]),
+	ETH: format(chartData[3].data[i])
+}))
